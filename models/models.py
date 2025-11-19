742e1d539f276ba68e32fe0a4827949373883808
Alex Feld <alex@amfeld.de>
2025-11-19 11:49:32 +0100

    Updated models.py

--- models/models.py
+++ models/models.py
@@ -96,7 +96,7 @@ class ProjectCostAggregator(models.Model):
                 WHERE
                     aaa.plan_id = (
                         SELECT id FROM account_analytic_plan
-                        WHERE LOWER(name) LIKE LOWER('%%projekt%%')
+                        WHERE LOWER(COALESCE(name::text, '')) LIKE LOWER('%%projekt%%')
                         LIMIT 1
                     )
                     AND aaa.active = TRUE
